0e8cebb8a5fe98a38690c7be0b09db820e627172
Felipe Blanco <feeh.blanco@gmail.com>
2023-05-22 14:52:35 -0300

    Meu primeiro projeto utilizando codeceptjs

---
+++ login_test.js
@@ -0,0 +1,37 @@
+Feature("login");
+
+Scenario("Login com sucesso", ({ I }) => {
+  I.amOnPage("http://automationpratice.com.br/");
+  I.click("Login");
+  I.waitForText("Login", 10);
+  I.fillField("#user", "felipeTeste@email.com");
+  I.fillField("#password", "pwd123");
+  I.click("#btnLogin");
+  I.waitForText("Login realizado", 3);
+}).tag("@sucesso");
+
+Scenario("Tentando Logar digitando apenas o e-mail", ({ I }) => {
+  I.amOnPage("http://automationpratice.com.br/");
+  I.click("Login");
+  I.waitForText("Login", 10);
+  I.fillField("#user", "felipeTeste@email.com");
+  I.click("#btnLogin");
+  I.waitForText("Senha inválida.", 3);
+});
+
+Scenario("Tentando logar sem digitar e-mail e senha", ({ I }) => {
+  I.amOnPage("http://automationpratice.com.br/");
+  I.click("Login");
+  I.waitForText("Login", 10);
+  I.click("#btnLogin");
+  I.waitForText("E-mail inválido.", 3);
+});
+
+Scenario("Tentando Logar digitando apenas a senha", ({ I }) => {
+  I.amOnPage("http://automationpratice.com.br/");
+  I.click("Login");
+  I.waitForText("Login", 10);
+  I.fillField("#password", "pwd123");
+  I.click("#btnLogin");
+  I.waitForText("E-mail inválido.", 3);
+});
